699889bc9819af50ed71d072b31280125b36feca
Gabriel Andreescu <gabriel@andreescu.dev>
2023-08-31 09:59:32 +0300

    feat: new products / small improvements

---
+++ src/components/SectionCatalogEventsGallery.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const gallery: string[] = [];
+
+for (let i = 1; i <= 9; i++) {
+  gallery.push('/gallery/events/' + i + '-min.jpg');
+}
+</script>
+
+<template>
+  <div class="columns-2xs gap-8 space-y-8">
+    <div class="relative aspect-w-24 aspect-h-9" v-for="image in gallery" :key="image">
+      <img class="w-full object-cover rounded-lg" :src="image" alt="imagine din galeria de evenimente" />
+      <div class="absolute inset-0 ring-1 ring-inset ring-black/10 rounded-lg"></div>
+    </div>
+  </div>
+</template>
